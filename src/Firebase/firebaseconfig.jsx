bab2bb4964557c6735892b520c557e027c1342b1
Sanjay Kumar <145492576+iamsanjaii@users.noreply.github.com>
2024-07-15 12:07:17 +0530

    Update firebaseconfig.jsx

--- src/Firebase/firebaseconfig.jsx
+++ src/Firebase/firebaseconfig.jsx
@@ -5,12 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA1778YZpVuFuUdm63mCM47yhhRNdJvRmA",
-  authDomain: "musicplayer-98a46.firebaseapp.com",
-  projectId: "musicplayer-98a46",
-  storageBucket: "musicplayer-98a46.appspot.com",
-  messagingSenderId: "168366407236",
-  appId: "1:168366407236:web:f65cbdff5776ae6cc80611",
+
 };
 
 // Initialize Firebase
